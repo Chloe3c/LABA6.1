44ca37204ff154f8cd81ac3d7ff74ca3bb7a9f63
Chloe3c <kristianishenko21@gmail.com>
2024-12-19 22:11:45 +0300

    Program.cs

--- LABA6.1/Program.cs
+++ LABA6.1/Program.cs
@@ -1,22 +1,31 @@
-﻿//Даны основание и высота двух равнобедренных трапеций.Найти сумму их периметров.(Определить функцию для расчета периметра равнобедренной трапеции по ее основанию и высоте).
 class Program
 {
-    static void Main()
+    static double CalculatePerimeter(double a, double b, double h)
     {
-        double a = 10; 
-        double b = 6;
-        double h = 4; 
-
-        double perimeter = CalculateTrapezoidPerimeter(a, b, h);
-        Console.WriteLine($"Периметр равнобедренной трапеции: {perimeter}");
+        double c = Math.Sqrt(h * h + Math.Pow(Math.Abs(a - b) / 2, 2)); 
+        double perimeter = a + b + 2 * c; 
+        return perimeter;
     }
 
-    static double CalculateTrapezoidPerimeter(double a, double b, double h)
+    static void Main(string[] args)
     {
-        double d = (a - b) / 2;
-        double l = Math.Sqrt(h * h + d * d);
-        double perimeter = a + b + 2 * l;
-
-        return perimeter;
+        Console.WriteLine("Введите длину первого основания первой трапеции (a1):");
+        double a1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите длину второго основания первой трапеции (b1):");
+        double b1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите высоту первой трапеции (h1):");
+        double h1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите длину первого основания второй трапеции (a2):");
+        double a2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите длину второго основания второй трапеции (b2):");
+        double b2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите высоту второй трапеции (h2):");
+        double h2 = Convert.ToDouble(Console.ReadLine());
+        double perimeter1 = CalculatePerimeter(a1, b1, h1);
+        double perimeter2 = CalculatePerimeter(a2, b2, h2);
+        double totalPerimeter = perimeter1 + perimeter2;
+        Console.WriteLine($"Периметр первой трапеции: {perimeter1:F2}");
+        Console.WriteLine($"Периметр второй трапеции: {perimeter2:F2}");
+        Console.WriteLine($"Сумма периметров двух трапеций: {totalPerimeter:F2}");
     }
 }
